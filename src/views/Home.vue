7c0febb933e36113f30b09cc1b6071bd43a5e57f
wangyuan <wangyuan@huidianyi.com>
2020-01-21 18:15:20 +0800

    2020.01

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="head width-100 height-60-px line-height-60-px b-text-cen">
+      <h3 class="priamy-color">2020-01-21</h3>
+    </div>
+    <div class="width-100 height-60-px mb-20"></div>
+    <el-row type="flex" justify="center">
+      <el-col
+        :xs="{ span: 22 }"
+        :sm="{ span: 20 }"
+        :md="{ span: 16 }"
+        :lg="{ span: 14 }"
+        :xl="{ span: 12 }"
+      >
+        <div class="nav width-100 height-40-px b-flex">
+          <div
+            v-for="item in navs"
+            :key="item.value"
+            class="height-100 line-height-40-px b-f-justify-cen b-text-cen width-100"
+          >
+            <span>{{ item.label }}</span>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      navs: [
+        { label: "主页", value: 1 },
+        { label: "主页", value: 2 }
+      ]
+    };
   }
 };
 </script>
+<style lang="scss" scoped>
+.home {
+  .head {
+    position: fixed;
+    top: 0;
+    right: 0;
+    box-shadow: 0px 6px 12px -4px rgba(144, 147, 153, 0.8);
+  }
+}
+</style>
