895cd08480e2b05e195fa6aadc71500e983b804c
wangyuan <wangy_60915950@163.com>
2020-02-03 10:23:53 +0800

    test分支合并master分支上的修复bug

--- src/views/gitLine.vue
+++ src/views/gitLine.vue
@@ -8,6 +8,8 @@
     <p>修改master分支上的内容2</p>
     <p>保存master分支上的内容</p>
     <p>此时我正在test分支上工作，突然又bug要解决，但是test分支上的工作还有完成，不想要提交，但是bug必需马上修改</p>
+    <p>修复master分支上的bug</p>
+    <p>修复master分支上的bug-不用no-off命令</p>
   </div>
 </template>
 
